dbbe1ade0b2e483fbfe8d28fbd672bab124d7b93
Kyle Richardson <kyle.richardson1@gmail.com>
2020-01-15 23:50:09 -0600

    added cors stretch

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const cors = require('cors')
 const db = require('./data/db')
 
 const server = express();
 server.use(express.json())
+server.use(cors())
 
 server.get('/', (req, res) => {
   res.send('Hello World');
